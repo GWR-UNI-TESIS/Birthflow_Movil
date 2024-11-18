d6fc22e74b62f6e1282cd5224d503c37ed3b3b93
Russell Soza <endosoza1@gmail.com>
2024-11-17 21:42:01 -0600

    Actualizando .env

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,7 @@ import Foundation
 import device_info_plus
 import flutter_secure_storage_macos
 import path_provider_foundation
-import sqflite
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
